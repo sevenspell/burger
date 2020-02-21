af052d98295802a2cb08c59d6cd74d89983cbf53
Seven <sevenspell@gmail.com>
2020-02-22 00:19:20 +1100

    prepared all backend files

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,9 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(100) NOT NULL,
+	id INT AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
