813b7620dee66b94e754bb2b4844f11969ab8cd9
Seven <sevenspell@gmail.com>
2020-02-21 23:24:46 +1100

    npm install and coded connection and orm

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon cheeseburger with special sauce');
+INSERT INTO burgers (burger_name) VALUES ('Swiss mushroom burger');
+INSERT INTO burgers (burger_name) VALUES ('Fish katsu burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken katsu burger');
+INSERT INTO burgers (burger_name) VALUES ('Kakiage rice burger');
