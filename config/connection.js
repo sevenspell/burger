ecd5c9e8207ab5e6174e80f8bd5bed79547764ef
Seven <sevenspell@gmail.com>
2020-02-27 23:19:57 +1100

    admin change

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL){
         host: "localhost",
         // port: 3306,
         user: "root",
-        password: "seven11spell$",
+        password: "Root11!!",
         database: "burgers_db"
     });
 }
